87dd096c25706dd7d4fff866541d5d54c418e183
Millybraga <emilly.braga02@hotmail.com>
2021-05-31 22:05:57 -0300

    primeiro commit - consumo API

--- BlogPessoal/src/app/cadastrar/cadastrar.component.ts
+++ BlogPessoal/src/app/cadastrar/cadastrar.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { User } from '../model/User';
+import { AuthService } from '../service/auth.service';
 
 @Component({
   selector: 'app-cadastrar',
@@ -7,9 +10,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CadastrarComponent implements OnInit {
 
-  constructor() { }
+  user: User = new User
+  confirmeSenha: string
+  tipoUser: string
 
-  ngOnInit(): void {
+  constructor(
+    private authService: AuthService,
+    private router: Router
+
+  ) { }
+
+  ngOnInit() {
+    window.scroll(0, 0) //X=0 e Y=0
+  }
+
+  ConfirmeSenha(event: any) {
+    this.confirmeSenha = event.target.value
+  }
+  TipoUser(event: any) {
+    this.tipoUser = event.target.value
+  }
+
+  Cadastrar() {
+    this.user.tipo = this.tipoUser
+    
+    if (this.user.senha != this.confirmeSenha) {
+      alert("As senhas estão incorretas")
+    } else {
+      this.authService.cadastrar(this.user).subscribe((resp: User) => {
+        this.user = resp
+        this.router.navigate(['/entrar'])
+
+        alert("Usuário cadastrado com sucesso!")
+      })
+    }
   }
 
 }
