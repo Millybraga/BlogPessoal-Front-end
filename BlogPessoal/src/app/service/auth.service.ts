abf4a5513149c988ef33542f558ffaab418994b0
Millybraga <emilly.braga02@hotmail.com>
2021-06-08 19:23:23 -0300

    Implementação do post e get de postagens

--- BlogPessoal/src/app/service/auth.service.ts
+++ BlogPessoal/src/app/service/auth.service.ts
@@ -23,6 +23,11 @@ export class AuthService {
     return this.http.post<User>(`${environment.server}/usuarios/cadastrar`, user)
   }
 
+  getByIdUser(id: number): Observable<User>{
+    return this.http.get<User>(`${environment.server}/usuarios/${id}`)
+  }
+
+
   logado(){
     let ok: boolean = false
 
